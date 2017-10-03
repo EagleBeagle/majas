1dc68a366b6953e1f1ec83727ff2b83f08e454d9
EagleBeagle <zolibiacsi@gmail.com>
2017-10-03 14:57:50 +0200

    majas application test added

---
+++ nagyonmajas/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/eaglebeagle/Code/Python/Django/nagyonmajas/nagyonmajas/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
